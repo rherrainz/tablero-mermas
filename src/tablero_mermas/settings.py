65a04a365371fd75fba82ea58b6291e10e58809b
Rodrigo Herrainz <rherrainz@gmail.com>
2025-11-05 15:36:38 -0300

    fixed settings

--- src/tablero_mermas/settings.py
+++ src/tablero_mermas/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import environ
 from django.core.management.utils import get_random_secret_key
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
